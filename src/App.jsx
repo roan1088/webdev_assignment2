e1e0e4c78fb276d372b6e8a32d665d2e32e527ef
roan1088 <rohan_188@yahoo.com>
2025-04-30 16:11:09 -0400

    Changed state to alternate between 5 colors

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [index, setIndex] = useState(0)
+  const colors = ["red", "green", "blue", "orange", "purple"];
+
+  const changeColor = () => {
+    setIndex((index + 1) % colors.length);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Current color is {colors[index]}</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={changeColor}>
+          Click to change color
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Colors: red, green, blue, orange and purple
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
